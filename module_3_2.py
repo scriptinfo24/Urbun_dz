57c021929d628d4df1220a56aa02401a9a040393
scriptinfo24 <scriptinfo24@gmail.com>
2024-11-16 20:17:46 +0700

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,18 +1,32 @@
 def send_email(message, recipient, *, sender="university.help@gmail.com"):
-
+    # Функция для проверки корректности email
 
     def is_valid_email(email):
-        valid = ('.com', '.ru', '.net')
-        return '@' in email and email.endswith(valid)
+        return isinstance(email, str) and ("@" in email) and (email.endswith(('.com', '.ru', '.net')))
+
+    # Проверка на корректность отправителя и получателя
 
     if not is_valid_email(sender) or not is_valid_email(recipient):
-        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
         return
 
-    # Здесь может быть код для отправки письма
-    print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient} с сообщением: {message}")
+    # Проверка на отправку самому себе
 
+    if sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+        return
 
+    # Проверка на отправителя по умолчанию
+
+    if sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+# Примеры выполнения функции
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
