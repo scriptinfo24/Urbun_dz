2a8c6f34eb21e76c7f4a811075e3c08aa8c8668f
scriptinfo24 <scriptinfo24@gmail.com>
2024-12-23 12:46:58 +0700

    Add files via upload
    8.11

--- 8/8.1.py
+++ 8/8.1.py
@@ -5,6 +5,6 @@ def add_everything_up(a, b):
         return str(a) + str(b)
 
 # Примеры использования
-print(add_everything_up(123.456, 'строка'))  # Вывод: 123.456строка
-print(add_everything_up('яблоко', 4215))      # Вывод: яблоко4215
-print(add_everything_up(123.456, 7))           # Вывод: 130.456
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
